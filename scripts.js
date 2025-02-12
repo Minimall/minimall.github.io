f31b70c7bf778dadd6379c91843a8c981ddc3cc2
Dmytro Dvornichenko <ebrillow@gmail.com>
2025-02-12 18:42:45 +0000

    Assistant checkpoint: Fix spaces in wave text effect
    Assistant generated file changes:
    - scripts.js: Fix spaces in wave text
    
    ---
    
    User prompt:
    
    still, link that says 'a personal story' appears as 'apersonalstory'

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ const setupHoverEffects = () => {
                 const needsWave = !element.querySelector('.wave-text');
                 if (needsWave) {
                     element.innerHTML = `<span class="wave-text">${
-                        text.split('').map(char => `<span>${char}</span>`).join('')
+                        text.split('').map(char => char === ' ' ? `<span>&nbsp;</span>` : `<span>${char}</span>`).join('')
                     }</span>`;
                 }
             }
